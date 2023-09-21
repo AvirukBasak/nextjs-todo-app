f2355d9d2f8abbd91f69983b8320dd6f55b13a85
AvirukBasak <avirukbasak@gmail.com>
2023-09-21 17:35:21 +0530

    updated app Head

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,9 +5,12 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Todo App</title>
-        <meta charSet="utf-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <title>NextJS Todo App</title>
+        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" sizes="any" type="image/svg+xml" href="favicon.svg" />
       </Head>
       <Form />
     </>
